2eff2a917311668e29d80cb5892c05a89bf739d6
robert morales <rmoralfit@gmail.com>
2024-10-13 15:12:35 -0400

    reconfigured openAIApi Api access .env file

---
+++ .env.js
@@ -0,0 +1,6 @@
+
+export const process = {
+    env: {
+        OPENAI_API_KEY:"sk-1kVwOYmmWk3MURBrIpbRcciM12WelPZlN3-AygI2plT3BlbkFJvSK8O0lS1oSNkjG_kQZKAXDV2Odxlco5w0Qag6POwA"
+    }
+}
--- index.js
+++ index.js
@@ -1,39 +1,47 @@
+import { process } from '/env';
+import { Configuration, OpenAIApi } from 'openai';
+
 
 const setupTextarea = document.getElementById('setup-textarea') 
 const setupInputContainer = document.getElementById('setup-input-container')
 const movieBossText = document.getElementById('movie-boss-text')
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
 
 document.getElementById("send-btn").addEventListener("click", () => {
- // if (setupTextarea.value) {
+  //if (setupTextarea.value) {
     setupInputContainer.innerHTML = `<img src="loading.svg" class="loading" id="loading">`
     movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
  // } 
+  fetchBotReply();
 })
 
-const apiKey = " "
-const url = "https://api.openai.com/v1/completions"
-
-fetch(url, {
-    method: 'POST',
-    headers: {
+ function fetchBotReply() {
+    fetch (url, {
+      method: 'POST',
+      headers: {
         'Content-Type': 'application/json' ,
-        'Authorization' : 'Bearer ${apiKey}'
+        'Authorization' : `Bearer ${apiKey}`
     }, 
-    body: JSON.stringify({
+      body: JSON.stringify({
         'model' : 'gpt-3.5-turbo-instruct' ,
-        'prompt' : "What is the capital of spain?"
+        'prompt' : "how do i let go of hurt in five words or less?"
     }),
-}).then(response => response.json().then(data => console.log(data)))
+  })   .then(response => response.json().then(data => 
+        movieBossText.innerText = data.choices[0].text
+  )) 
+} 
 
 
-
-/*
-
+/* 
 import fetch from 'node-fetch';
 
-  // Import node-fetch for Node.js
+   //Import node-fetch for Node.js
 
-const apiKey = " ";
+const apiKey = "";
 const url = "https://api.openai.com/v1/completions";
 const setupInputContainer = document.getElementById('setup-input-container');
 const movieBossText = document.getElementById('movie-boss-text');
